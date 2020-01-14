509df44b783e234db6437ca5f788a77c7ee01f25
Nghia Le <nghiale@flomail.net>
2020-01-14 16:38:59 +0700

    updated

---
+++ get_version.py
@@ -0,0 +1,10 @@
+from jenkinsapi.jenkins import Jenkins
+
+def get_server_instance():
+    jenkins_url = 'https://jenkins.flodev.net'
+    server = Jenkins(jenkins_url, username='admin', password='Nghial@1')
+    return server
+
+if __name__ == '__main__':
+    # print get_server_instance().version
+    print 
